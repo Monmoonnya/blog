9b9b6353890f062cdd8f099485984397f87ba25a
Monmoon <2067589558a@gmail.com>
2025-11-25 16:16:34 +0800

    add deploy.yml for github action

--- constant.ts
+++ constant.ts
@@ -3,9 +3,9 @@ import type { PostModel } from './src/interfaces/post-model'
 export const config = {
   site: 'https://astro-blog-theme.vercel.app',
   description:
-    'Astro Blog Theme 是一个基于 Astro 的博客主题，支持 PWA、搜索、深色模式等功能，提供完整的 Markdown 语法支持。',
-  siteName: 'Astro Blog Theme',
-  author: 'Astro Blog Theme',
+    '梦话留到梦里说。',
+  siteName: 'MonmoonのBlog',
+  author: 'Monmoon',
   language: 'zh-CN',
 
   tip: '检测到页面内容有更新，是否刷新页面',
