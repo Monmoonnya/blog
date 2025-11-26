476b619355999ac534b21a9446578294869b3a76
Monmoon <2067589558a@gmail.com>
2025-11-26 13:17:02 +0800

    delete tips

--- constant.ts
+++ constant.ts
@@ -1,16 +1,12 @@
 import type { PostModel } from './src/interfaces/post-model'
 
 export const config = {
-  site: 'https://astro-blog-theme.vercel.app',
+  site: 'https:/',
   description:
     '梦话留到梦里说。',
   siteName: 'MonmoonのBlog',
   author: 'Monmoon',
   language: 'zh-CN',
-
-  tip: '检测到页面内容有更新，是否刷新页面',
-  tip_confirm: '是',
-  tip_cancel: '否',
 
   postsPerPage: 12,
   linkAttr: 'abbrlink',
